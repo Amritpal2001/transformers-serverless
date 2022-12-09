a5bab58888a4343ad2ae2065b2d038fea6df558c
Amritpal Singh <amrit@Amritpals-MacBook-Air.local>
2022-12-10 00:38:53 +0530

    test1

--- api/index.py
+++ api/index.py
@@ -1,15 +1,21 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+from transformers import GPT2TokenizerFast
+tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+
+
+def count_tokens(text) -> int:
+        return len(tokenizer(text)["input_ids"])
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        s = self.path
-        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+    def do_POST(self):
+        # Parse the form data
+        form = parse.parse_qs(self.rfile.read(int(self.headers['Content-Length'])))
+
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        if "name" in dic:
-            message = "Hello, " + dic["name"] + "!"
-        else:
-            message = "Hello, stranger!"
+        if "text" in form:
+            print(form["text"][0])
+            message = {"tokens": count_tokens(form["text"][0])}
         self.wfile.write(message.encode())
         return
