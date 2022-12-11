9b19faa2ec661497b0846c47f852ef758921dde1
Amritpal Singh <amrit@Amritpals-MacBook-Air.local>
2022-12-11 20:06:34 +0530

    test1

--- api/handler.js
+++ api/handler.js
@@ -4,9 +4,8 @@ export default function handler(request, response) {
     if (request.method === 'POST') {
       // Get the value of the "text" parameter from the request body
       const text = request.body.text;
-      return response.end(encode(text).length);
+      return response.status(200).json({"tokens": encode(text).length});
     } else {
       return response.status(405).end(); // Method Not Allowed
     }
   }
-  
