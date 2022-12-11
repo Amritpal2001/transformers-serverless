78d202582a79c95fcaf772b9a958d4f8ffed3100
Amritpal Singh <amrit@Amritpals-MacBook-Air.local>
2022-12-11 19:46:31 +0530

    add nodejs

---
+++ api/handler.js
@@ -0,0 +1,12 @@
+const { encode } = require('gpt-3-encoder')
+
+export default function handler(request, response) {
+    if (request.method === 'POST') {
+      // Get the value of the "text" parameter from the request body
+      const text = request.body.text;
+      return response.end(encode(text).length);
+    } else {
+      return response.status(405).end(); // Method Not Allowed
+    }
+  }
+  
