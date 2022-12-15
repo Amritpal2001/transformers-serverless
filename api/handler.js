3310f5eafa5fe262a659ddb7aa743c14c1fa90e4
Amritpal Singh <amrit@Amritpals-MacBook-Air.local>
2022-12-15 10:33:02 +0530

    add only for get request

--- api/handler.js
+++ api/handler.js
@@ -1,9 +1,9 @@
 const { encode } = require('gpt-3-encoder')
 
 export default function handler(request, response) {
-    if (request.method === 'POST') {
-      // Get the value of the "text" parameter from the request body
-      const text = request.body.text;
+    if (request.method === 'GET') {
+      // Get the value of the "text" parameter from the query string
+      const text = request.query.text;
       return response.status(200).json({"tokens": encode(text).length});
     } else {
       return response.status(405).end(); // Method Not Allowed
