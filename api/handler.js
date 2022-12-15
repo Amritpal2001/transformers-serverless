c94d1943344b526249e23b8e428992e70c8d7a18
Amritpal Singh <amrit@Amritpals-MacBook-Air.local>
2022-12-15 10:36:09 +0530

    handle indefined

--- api/handler.js
+++ api/handler.js
@@ -3,7 +3,10 @@ const { encode } = require('gpt-3-encoder')
 export default function handler(request, response) {
     if (request.method === 'GET') {
       // Get the value of the "text" parameter from the query string
-      const text = request.query.text;
+      const text = request?.query?.text;
+      if (!text) {
+        return response.status(400).end(); // Bad Request
+      }
       return response.status(200).json({"tokens": encode(text).length});
     } else {
       return response.status(405).end(); // Method Not Allowed
